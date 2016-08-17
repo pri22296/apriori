23e272038f64a2df37fe82e2dd6d986cce0440e7
Priyam Singh <priyamsingh.22296@gmail.com>
2016-08-17 20:05:06 +0530

    added plot for accuracy-confidence_threshold

--- plotter.py
+++ plotter.py
@@ -3,11 +3,13 @@ import apriori
 import time
 
 support_threshold_list = [i/100 for i in range(1,40)]
+confidence_threshold_list = [i/20 for i in range(1,20)]
 accuracy_list = []
 
 confidence_threshold = 0.2
 coverage_threshold = 5
 top_k_rules = 20
+support_threshold = 0.07
 
 print("top_k_rules is " + str(top_k_rules))
 print("Confidence Threshold is " + str(confidence_threshold))
@@ -34,7 +36,24 @@ for sup_th in support_threshold_list:
     accuracy_list.append(time_taken)
     
 plt.plot(support_threshold_list, accuracy_list)
-plt.ylabel('time taken to learn')
+plt.ylabel('time taken to learn(seconds)')
 plt.xlabel('support_threshold')
 plt.axis([0, 0.4, 0, 20])
+plt.show()
+
+print("top_k_rules is " + str(top_k_rules))
+print("Support Threshold is " + str(support_threshold))
+print("Coverage Threshold is " + str(coverage_threshold))
+
+accuracy_list = []
+
+for conf_th in confidence_threshold_list:
+    accuracy = apriori.test(*apriori.learn(support_threshold, conf_th, coverage_threshold), top_k_rules)
+    print("accuracy for confidence_threshold {} is {}".format(conf_th, accuracy))
+    accuracy_list.append(accuracy)
+
+plt.plot(confidence_threshold_list, accuracy_list)
+plt.ylabel('accuracy')
+plt.xlabel('confidence_threshold')
+plt.axis([0, 1, 0, 100])
 plt.show()
