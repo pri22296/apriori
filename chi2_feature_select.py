7a9b8d3c008de31c49be390cdca146ee8d773a06
sameersingh7 <sam7thegreat@gmail.com>
2016-08-28 00:43:39 +0530

    Updated chi2_fature_select.py

--- chi2_feature_select.py
+++ chi2_feature_select.py
@@ -3,6 +3,9 @@ import apriori
 dataset = apriori.get_dataset_from_file('Itemset_train.txt')
 classes = apriori.get_classes_from_file('Classes_train.txt')
 
+chi2_dist = [5.024, 7.378, 9.348, 11.143, 12.833, 14.449, 16.013, 17.535, 19.023,
+20.483, 21.920, 23.337, 24.736, 26.119, 27.488, 28.845, 30.191,31.526, 32.852, 34.170]
+
 feature_options_list = list(map(set, zip(*dataset)))
 classes_list = list(set(classes))
 
@@ -14,7 +17,7 @@ def weird_sum(l,index):
 
 for feature_options in feature_options_list:
     degree = (len(feature_options) - 1) * (len(classes_list) - 1)
-    print(degree)
+    #print(degree)
     print(feature_options)
     expect = []
     chi = 0
@@ -27,6 +30,8 @@ for feature_options in feature_options_list:
             expected = k[1]*ws0/ws1
             observed = k[0]
             chi += (observed-expected)**2/expected
-        print(l)
-    print(chi)
+        #print(l)
+    #print(chi)
+    chi_diff = abs(chi - chi2_dist[degree-1])
+    print("Chi Difference = " + str(chi_diff))
         
