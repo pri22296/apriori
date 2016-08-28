d80a2458ef885da706e5ae56afa8a2dda3e4e7aa
Priyam Singh <priyamsingh.22296@gmail.com>
2016-08-28 15:45:13 +0530

    proper merge for previous conflict

--- chi2_feature_select.py
+++ chi2_feature_select.py
@@ -1,9 +1,8 @@
 import apriori
 
-chi2 = [3.841, 5.991, 7.815, 9.488, 11.070, 12.592, 14.067, 15.507, 16.919,
-        18.307, 19.675, 21.026, 22.362, 23.685, 24.996, 26.296, 27.587, 28.869,
-        30.144, 31.410, 32.671, 33.924, 35.172, 36.415, 37.652, 38.885, 40.113,
-        41.337, 42.557, 43.773]
+chi2_dist = [5.024, 7.378, 9.348, 11.143, 12.833, 14.449, 16.013, 17.535, 19.023,
+             20.483, 21.920, 23.337, 24.736, 26.119, 27.488, 28.845, 30.191,
+             31.526, 32.852, 34.170]
 
 dataset = apriori.get_dataset_from_file('Itemset_train.txt')
 classes = apriori.get_classes_from_file('Classes_train.txt')
@@ -31,9 +30,8 @@ for feature_options in feature_options_list:
             expected = k[1]*ws0/ws1
             observed = k[0]
             chi += (observed-expected)**2/expected
-        print(l)
-    chi_critical = chi2[degree-1]
-    print(round(chi, 3), chi_critical)
+    chi_critical = chi2_dist[degree-1]
+    print("chi2_stats = {}, chi2_critical_value = {}".format(round(chi, 3), chi_critical))
     if(chi > chi_critical):
         print("Important\n")
     else:
